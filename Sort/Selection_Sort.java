69009200ad53ac560a931dbb6eb5356a7b029a97
Jiajie Yan <svenyan234@gmail.com>
2017-11-06 11:25:40 -0500

    Update Selection_Sort.java

--- Sort/Selection_Sort.java
+++ Sort/Selection_Sort.java
@@ -1,8 +1,7 @@
-package sort;
 
 /*
  * Search entire list to get minimum, swap to head to form sorted sublist,
- * do the same for the rest unsorted sublist.
+ * do the same for the rest unsorted sublist. Happen in place, no new list needed.
  * Time complexity: best O(n), worst = average = O(n^2), stable. 
  */
 
@@ -11,26 +10,29 @@ public class Selection_Sort {
 	public static void main(String[] args) {
 		int[] arr = {4,2,5,7,9,8,3,1,6,0};
 		Selection_Sort ss = new Selection_Sort();
-		int[] res = ss.sort(arr);
-		for (int i = 0; i < res.length; i++) System.out.println(res[i]);
+		ss.sort(arr);
+		for (int i = 0; i < arr.length; i++) System.out.println(arr[i]);
 
 	}
 	
-	public int[] sort(int[] arr) {
-		for (int i = 1; i < arr.length; i++) {      //start point of each loop
+	public void sort(int[] arr) {
+		//start point of each loop, each iteration increases 1 because in last iteration we found the minimum one and put it in the sorted list, whose length += 1
+		for (int i = 1; i < arr.length; i++) {
+			//always assume minimum item is the first item at first in each iteration
 			int min = i;
-			for (int j = i; j < arr.length; j++) {  //go through the whole unsorted sublist
+			//go through the whole unsorted sublist and find out the sublist's minimum item's index
+			for (int j = i; j < arr.length; j++) {
 				if (arr[j] < arr[min]) {
 					min = j;
 				}
 			}
+			//swap values if the last element in sorted list is < min
 			if (arr[i-1] > arr[min]) {
 				int temp = arr[i-1];
 				arr[i-1] = arr[min];
 				arr[min] = temp;
 			}
 		}
-		return arr;
 	}
 
 }
