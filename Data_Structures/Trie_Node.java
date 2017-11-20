85bf87e1c878b75bf5fa06852c3af31e9ffa51e5
Jiajie Yan <svenyan234@gmail.com>
2017-11-19 23:10:07 -0500

    Update Trie_Node.java

--- Data_Structures/Trie_Node.java
+++ Data_Structures/Trie_Node.java
@@ -1,6 +1,8 @@
 package data_structures;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Trie_Node {
@@ -13,5 +15,24 @@ public class Trie_Node {
 	
 	public Trie_Node(char c) {
 		this.c = c;
+	}
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		List<String> list = new ArrayList<>();
+		showWords(this, sb, list);
+		return String.join(", ", list);
+	}
+	
+	public void showWords(Trie_Node root, StringBuilder sb, List<String> list){
+		if (root == null) return;
+		for (Map.Entry<Character, Trie_Node> entry : root.children.entrySet()) {
+			Character ch = entry.getKey();
+			Trie_Node node = entry.getValue();
+			sb.append(ch);
+			if (node.isEnd) list.add(sb.toString());
+			showWords(node, sb, list);
+			sb.deleteCharAt(sb.length()-1);
+		}
 	}
 }
