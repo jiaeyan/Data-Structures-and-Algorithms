59b5fb5c340835a9c0013ee9219ca3e2b678e6ec
Jiajie Yan <svenyan234@gmail.com>
2018-01-08 23:54:07 -0500

    Update BucketSort.java

--- Sort/IntegerSort/BucketSort.java
+++ Sort/IntegerSort/BucketSort.java
@@ -5,17 +5,18 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
+/*
+ * Stable.
+ * 
+ * Complexity: O(n)
+ */
+
 public class BucketSort {
 
 	int bucketSize = 10;
     int arraySize = 10000;
-
-    public static void main(String[] args) {
-        BucketSort bs = new BucketSort();
-        int[] array = bs.getArray();
-        bs.bucketSort(array);
-    }
-
+    
+    // Randomly generate an array to be sorted.
     public int[] getArray() {
         int[] arr = new int[arraySize/3];
         Random r = new Random();
@@ -24,9 +25,13 @@ public class BucketSort {
         }
         return arr;
     }
-
+    
     public void bucketSort(int[] a) {
+    	    // Step 1: create buckets.
         List<Integer> bucket[] = new ArrayList[bucketSize];
+        
+        // Step 2: put elements of array to relative buckets
+        //         according to some hash function.
         for (int i = 0; i < a.length; i++) {
             int temp = a[i] / 10000;
             if (bucket[temp] == null) {
@@ -34,6 +39,8 @@ public class BucketSort {
             }
             bucket[temp].add(a[i]);
         }
+        
+        // Step 3: sort each bucket and print the result.
         for (int j = 0; j < bucketSize; j++) {
             insertSort(bucket[j]);
             printList(bucket[j]);
@@ -42,12 +49,18 @@ public class BucketSort {
 
     public void printList(List<Integer> list) {
         while (list.size() > 0) {
-            System.out.print(list.remove(0) + "\t");
+            System.out.println(list.remove(0));
         }
     }
 
     public void insertSort(List<Integer> list) {
         Collections.sort(list);
+    }
+    
+    public static void main(String[] args) {
+        BucketSort bs = new BucketSort();
+        int[] array = bs.getArray();
+        bs.bucketSort(array);
     }
 
 }
